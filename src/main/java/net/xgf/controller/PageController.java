f9cc4c987ea5c722e21506a45956fd90e3d3c568
tony1989zhang <zhangcanneng@sina.com>
2020-05-11 15:43:30 +0800

    提交

--- src/main/java/net/xgf/controller/PageController.java
+++ src/main/java/net/xgf/controller/PageController.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
-	/**
-	 * 
-	 * @return
-	 */
 	@RequestMapping("/")
 	public String showIndex() {
 		return "index";
